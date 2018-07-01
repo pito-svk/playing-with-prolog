9660263b472edbd00fae9aad3b366252d3f92c07
Peter Parada <parad.peter@gmail.com>
2018-07-01 23:22:02 +0200

    Add solution for third problem

--- 99_problems.pl
+++ 99_problems.pl
@@ -5,3 +5,8 @@ solve_1(X, [_|Xs]) :- solve_1(X, Xs).
 
 solve_2(X, [X, _]).
 solve_2(X, [_|Xs]) :- solve_2(X, Xs).
+
+solve_3(X, [X|_], 1).
+solve_3(X, [_|Xs], K) :-
+  K_dec is K - 1,
+  solve_3(X, Xs, K_dec).
