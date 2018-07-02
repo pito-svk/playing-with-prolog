7cfc481369bd00f96a3122033b37fa0ce0c0a9ff
Peter Parada <parad.peter@gmail.com>
2018-07-02 13:49:13 +0200

    Add solution for problem 5

--- 99_problems.pl
+++ 99_problems.pl
@@ -20,3 +20,6 @@ solve_4([_|Xs], L, C) :-
   C_int is C + 1,
   solve_4(Xs, L, C_int).
 solve_4([_], L, C) :- L is C + 1.
+
+solve_5([X, Y], R) :- R = [Y, X].
+solve_5([X|Xs], R) :- solve_5(Xs, R_tail), R = [R_tail | X].
