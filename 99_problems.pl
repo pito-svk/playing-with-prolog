bd78ff9967b85de7ac06c76aa7bd3859cdfb4dca
Peter Parada <parad.peter@gmail.com>
2018-07-02 13:44:48 +0200

    Add solution 4

--- 99_problems.pl
+++ 99_problems.pl
@@ -10,3 +10,13 @@ solve_3(X, [X|_], 1).
 solve_3(X, [_|Xs], K) :-
   K_dec is K - 1,
   solve_3(X, Xs, K_dec).
+
+solve_4([], 0).
+solve_4([_], 1).
+solve_4([_|Xs], L) :-
+  solve_4(Xs, L, 1).
+
+solve_4([_|Xs], L, C) :-
+  C_int is C + 1,
+  solve_4(Xs, L, C_int).
+solve_4([_], L, C) :- L is C + 1.
